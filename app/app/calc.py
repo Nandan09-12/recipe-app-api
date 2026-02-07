880eb1debeb68349ba311de8b92bf584a282f193
Nandan <yedunandan98@gmail.com>
2026-02-06 20:09:56 -0600

    Added DB to adapter

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator Function
+"""
+
+
+def add(x, y):
+    """Return the sum of x and y."""
+    return x + y
+
+
+def subtract(x, y):
+    """Return the result of subtracting y from x."""
+    return x - y
+
+
